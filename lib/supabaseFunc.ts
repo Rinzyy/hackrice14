1502ff3e37c03d6f4968a6f44105f536dd4ef5ab
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-09-19 22:20:54 -0500

    finished with connecting supabase to nextjs

---
+++ lib/supabaseFunc.ts
@@ -0,0 +1,12 @@
+
+import supabase from "./supabase";
+
+export default async function readItems() {
+    const { data, error } = await supabase.from('test').select('*');
+
+    if (error) {
+      console.error('Error reading items:', error);
+    } else {
+      console.log('Items:', data);
+    }
+  }
