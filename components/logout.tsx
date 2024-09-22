74a247245e243979d3806876a0b05547614e726f
Rinzy <atermizor@gmail.com>
2024-09-21 21:03:16 -0500

    Update logout.tsx

--- components/logout.tsx
+++ components/logout.tsx
@@ -2,10 +2,13 @@
 import supabase from '@/lib/supabase';
 import React from 'react';
 import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
 
 export default function Logout() {
+	const router = useRouter();
 	const logg = async () => {
 		await supabase.auth.signOut();
+		router.push('/login');
 	};
 	return (
 		<Button
