a7d31ca73c9925b590cb177cf1c2166aaa913737
Rinzy <atermizor@gmail.com>
2024-09-21 21:39:37 -0500

    appointment set

--- components/leftside.tsx
+++ components/leftside.tsx
@@ -1,6 +1,8 @@
+import { fetchLatestApp } from '@/lib/supabaseFunc';
 import React from 'react';
 
-export default function Leftside({ appointment = null }) {
+export default async function Leftside() {
+	const appData = await fetchLatestApp();
 	return (
 		<aside className="bg-white p-6 rounded-lg border-2 h-auto shadow-md max-w-sm">
 			<section className="mb-6">
@@ -8,7 +10,8 @@ export default function Leftside({ appointment = null }) {
 					Chat Information
 				</h2>
 				<p className="text-lg mb-2">
-					You are talking to <span className="font-semibold">Doctor John</span>
+					You are talking to{' '}
+					<span className="font-semibold">AI of Doctor John</span>
 				</p>
 				<p className="text-sm text-muted-foreground">
 					You can ask any questions related to your previous medical history,
@@ -16,17 +19,26 @@ export default function Leftside({ appointment = null }) {
 				</p>
 			</section>
 			<section>
-				<h2 className="text-xl font-bold mb-2 text-primary">Appointment</h2>
-				{appointment ? (
-					<p className="text-sm">
-						Your next appointment is scheduled for{' '}
-						<span className="font-semibold">{appointment}</span>
-					</p>
-				) : (
-					<div className="bg-secondary p-4 rounded-md">
-						<p className="text-sm font-bold">
-							You currently have no scheduled appointments.
+				{appData ? (
+					<div className=" border-2 border-green-500 rounded-lg p-4 ">
+						<h2 className="text-xl text-green-500 font-bold mb-2 text-primary">
+							Appointment
+						</h2>
+						<p className="text-sm">
+							Your next appointment is scheduled for{' '}
+							<span className="font-semibold">{appData.date}</span>
 						</p>
+					</div>
+				) : (
+					<div>
+						<h2 className="text-xl  font-bold mb-2 text-primary">
+							Appointment
+						</h2>
+						<div className="bg-secondary p-4 rounded-md">
+							<p className="text-sm font-bold">
+								You currently have no scheduled appointments.
+							</p>
+						</div>
 					</div>
 				)}
 			</section>
--- lib/supabaseFunc.ts
+++ lib/supabaseFunc.ts
@@ -1,11 +1,44 @@
+import { revalidatePath } from 'next/cache';
 import supabase from './supabase';
 
-export default async function readItems() {
+export async function readItems() {
 	const { data, error } = await supabase.from('test').select('*');
 
 	if (error) {
 		console.error('Error reading items:', error);
 	} else {
 		console.log('Items:', data);
+	}
+}
+
+export async function fetchLatestApp() {
+	const { data, error } = await supabase
+		.from('appointments')
+		.select('*')
+		.order('created_at', { ascending: false })
+		.limit(1)
+		.single();
+
+	if (error) {
+		console.error('Error fetching latest appointment:', error);
+		return null;
+	} else {
+		console.log('Latest appointment:', data);
+		return data;
+	}
+}
+
+export async function createNewAppointment(date: string, description: string) {
+	const { error } = await supabase
+		.from('appointments')
+		.insert({ date, description });
+
+	if (error) {
+		console.error('Error fetching latest appointment:', error);
+		return null;
+	} else {
+		console.log('Created appointment');
+		revalidatePath('/dashboard/chat');
+		return 'success';
 	}
 }
