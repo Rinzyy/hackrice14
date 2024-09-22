28e8e992a1f005a34522e707b934a98625ac43e4
Huy Vo <huy.businesscontact@gmail.com>
2024-09-22 02:45:26 -0500

    added a calender to the left side

--- components/leftside.tsx
+++ components/leftside.tsx
@@ -1,11 +1,12 @@
 import { fetchLatestApp } from '@/lib/supabaseFunc';
 import React from 'react';
 import AppAvailable from './appAvailable';
+import BasicDateCalendar from './ui/calendar';
 
 export default async function Leftside() {
 	const appData = await fetchLatestApp();
 	return (
-		<aside className="bg-white p-6 rounded-lg border-2 h-auto shadow-md max-w-sm">
+		<aside className="bg-white p-6 rounded-lg border-2 h-auto shadow-md max-w-sm ml-16">
 			<section className="mb-6">
 				<h2 className="text-xl font-bold mb-2 text-primary">
 					Chat Information
@@ -20,6 +21,10 @@ export default async function Leftside() {
 				</p>
 			</section>
 			<AppAvailable appData={appData} />
+			<section className="mt-6">
+				<h2 className="text-xl font-bold mb-2 text-primary">Calendar</h2>
+				<BasicDateCalendar />
+			</section>
 		</aside>
 	);
 }
